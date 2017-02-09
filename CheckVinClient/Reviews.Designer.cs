2dcfebfa29acff7e0f4cd26034aa32c193dc6250
Michał <michal.dwojak92@gmail.com>
2017-02-09 23:55:58 +0100

    Add - form "reviews" method: pdf creation

--- CheckVinClient/Reviews.Designer.cs
+++ CheckVinClient/Reviews.Designer.cs
@@ -28,12 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Reviews));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -49,16 +53,26 @@
             // 
             // dataGridView1
             // 
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle4;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle5;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle6;
             this.dataGridView1.Location = new System.Drawing.Point(0, 34);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
@@ -86,6 +100,21 @@
             this.button1.TabIndex = 31;
             this.button1.Text = "Print";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.Visible = false;
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
             // 
             // Reviews
             // 
@@ -115,5 +144,7 @@
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
+        private System.Drawing.Printing.PrintDocument printDocument1;
     }
 }
--- CheckVinClient/Reviews.cs
+++ CheckVinClient/Reviews.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,14 @@ namespace CheckVinClient
 {
     public partial class Reviews : Form
     {
+
+        PaperSize paperSize = new PaperSize("papersize", 850, 1350);
+        int Number = 0;
+        int PerPageItem = 0;
         public Reviews()
         {
             InitializeComponent();
+
 
             WCFVIN.Service1Client con = new WCFVIN.Service1Client();
             DataSet response = con.ShowReviewsTable(Global.VIN);
@@ -28,6 +34,91 @@ namespace CheckVinClient
                 Global.Error = 0;
 
             }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PerPageItem = Number = 0;
+            printPreviewDialog1.Document = printDocument1;
+            ((ToolStripButton)((ToolStrip)printPreviewDialog1.Controls[1]).Items[0]).Enabled = false;
+            printDocument1.DefaultPageSettings.PaperSize = paperSize;
+            printPreviewDialog1.ShowDialog();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            e.Graphics.DrawImage(Properties.Resources._2017_02_06_at_03_47, 20, 10, 800, 140);
+            e.Graphics.DrawString("_ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, 140));
+            e.Graphics.DrawString("Selected VIN: " + Global.VIN + "", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(270, 170));
+            e.Graphics.DrawString("Date create report: " + DateTime.Now + "", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(250, 190));
+            e.Graphics.DrawString("__________________________________________________________________________________", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, 200));
+
+            int currentY = 240;
+
+            while (Number <= dataGridView1.Rows.Count - 3)
+            {
+                //cells info
+                //0 - Review date
+                //1 - Workshop name
+                //2 - Workshop adress
+                //3 - Mileage
+                //4 - Car colour
+                //5 - Fuel
+                //6 - Employeee name with workshop
+                //7 - Brakes
+                //8 - Damper
+                //9 - Exhaust 
+                //10 - Convergence
+                //11 - Light
+                //12 - VIN
+                e.Graphics.DrawString("Nr.: " + (Number + 1), new Font("Arial", 14, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 40;
+                e.Graphics.DrawString("Workshop name: " + dataGridView1.Rows[Number].Cells[1].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                e.Graphics.DrawString("Review date: " + dataGridView1.Rows[Number].Cells[0].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(543, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Workshop address: " + dataGridView1.Rows[Number].Cells[2].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                e.Graphics.DrawString("VIN number: " + dataGridView1.Rows[Number].Cells[12].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(543, currentY));
+                e.Graphics.DrawString("____________________________", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(543, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Employee: " + dataGridView1.Rows[Number].Cells[6].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 40;
+                e.Graphics.DrawString("Car Colour: " + dataGridView1.Rows[Number].Cells[4].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Mileage: " + dataGridView1.Rows[Number].Cells[3].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Fuel: " + dataGridView1.Rows[Number].Cells[5].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Brakes: " + dataGridView1.Rows[Number].Cells[7].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Damper: " + dataGridView1.Rows[Number].Cells[8].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Exhaust: " + dataGridView1.Rows[Number].Cells[9].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Convergence: " + dataGridView1.Rows[Number].Cells[10].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+                e.Graphics.DrawString("Light: " + dataGridView1.Rows[Number].Cells[11].Value.ToString(), new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+                currentY += 20;
+
+                e.Graphics.DrawString("________________________________________________________________________________", new Font("Arial", 12, FontStyle.Regular), Brushes.Black, new Point(25, currentY));
+               
+             
+                currentY += 70;
+                Number += 1;
+
+                if (PerPageItem < 2)
+                {
+                    PerPageItem += 1;
+                    e.HasMorePages = false;
+                }
+                else
+                {
+                    PerPageItem = 0;
+                    e.HasMorePages = true;
+                    return;
+                }
+            }
+
         }
     }
 }
